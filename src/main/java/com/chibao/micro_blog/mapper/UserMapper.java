603c65adec2a33c1eea75137afb272aa28d4f09c
ChiBao <baotochi87@gmail.com>
2025-09-25 17:59:35 +0700

    docs: add spring boot devtoosl

--- src/main/java/com/chibao/micro_blog/mapper/UserMapper.java
+++ src/main/java/com/chibao/micro_blog/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package com.chibao.micro_blog.mapper;
 
 import com.chibao.micro_blog.dto.request.UserCreationRequest;
+import com.chibao.micro_blog.dto.response.UserResponse;
 import com.chibao.micro_blog.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -8,4 +9,6 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface UserMapper {
     User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User user);
 }
